de27a811d1e6a911c545700260dba57dbf8d00b0
HANSOL PARK <ruflthfl00@naver.com>
2022-11-04 18:24:48 +0900

    2022. 11. 04.

--- src/main/webapp/5_jquery_class/1_css_event_2/js/jquery.js
+++ src/main/webapp/5_jquery_class/1_css_event_2/js/jquery.js
@@ -1,16 +1,17 @@
 $(function(){
    //이미지에 마우스를 올리거나 내릴 때 : hover( function, function )
-   $('img').hover(function(){
+   $('.header > .menu img').hover(function(){
    /*마우스를 올렸을 때*/
-<<<<<<< HEAD
-      var src = $(this).attr('src');                  //이미지 주소값 불러와서
+
+      var src = $(this).attr('src');                  //이미지 속성값 불러와서
       $(this).attr('src',src.replace('off','on'));    //off를 on으로 바꿔줌
-=======
+	},
+	/*	
    function(){
       var src = $(this).attr('src');                  //이미지 주소값 불러오기(.attr)
       $(this).attr('src',src.replace('off','on'));    //off → on으로 바꿔줌(.attr(a, b))
->>>>>>> e02e9149dd2ff15d89a9a19829f70a13fee92401
-   },
+   }*/
+   
    /*마우스를 내렸을 때*/
    function(){
       var src = $(this).attr('src');                  //이미지 주소값 불러오기(.attr)
